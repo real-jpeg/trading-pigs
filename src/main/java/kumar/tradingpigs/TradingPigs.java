a28148d2ce5e917604f82e590767660b03f9a8cc
k_umar <fishfearniko@gmail.com>
2025-07-19 16:47:39 +0400

    initial stuff

--- src/main/java/kumar/tradingpigs/TradingPigs.java
+++ src/main/java/kumar/tradingpigs/TradingPigs.java
@@ -1,19 +1,18 @@
-package turniplabs.examplemod;
+package kumar.tradingpigs;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.util.ClientStartEntrypoint;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class ExampleMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+public class TradingPigs implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
     public static final String MOD_ID = "examplemod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
-        LOGGER.info("ExampleMod initialized.");
+        LOGGER.info("TradingPigs initialized.");
     }
 
 	@Override
